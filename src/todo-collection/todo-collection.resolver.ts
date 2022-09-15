e1b4088a7fee1e4dec0964a770b87fe52468e2a9
Alexander Matvienko <01antizyk.it@gmail.com>
2022-09-15 17:41:35 +0300

    add todo collection

--- src/todo-collection/todo-collection.resolver.ts
+++ src/todo-collection/todo-collection.resolver.ts
@@ -1,8 +1,38 @@
-import {Resolver} from '@nestjs/graphql';
+import {Args, Mutation, Query, Resolver} from '@nestjs/graphql';
 import {TodoCollection} from "./todo-collection.model";
 import {TodoCollectionService} from "./todo-collection.service";
+import {Schema as MongooseSchema} from 'mongoose';
+import {CreateTodoCollectionInput, ListTodoCollectionInput, UpdateTodoCollectionInput} from "./todo-collection.inputs";
 
 @Resolver(() => TodoCollection)
 export class TodoCollectionResolver {
   constructor(private todoCollectionService: TodoCollectionService) {}
+
+  @Query(() => TodoCollection)
+  async todoCollection(@Args('_id', {type: () => String}) _id: MongooseSchema.Types.ObjectId) {
+    return this.todoCollectionService.getById(_id)
+  }
+
+  @Query(() => [TodoCollection])
+  async todoCollections (@Args('filters', {nullable: true}) filters?: ListTodoCollectionInput) {
+    return this.todoCollectionService.list(filters)
+  }
+
+  @Mutation(() => TodoCollection)
+  async createTodoCollection(@Args('payload') payload: CreateTodoCollectionInput) {
+    return this.todoCollectionService.create(payload)
+  }
+
+  @Mutation(() => TodoCollection)
+  async updateTodoCollection(@Args('payload') payload: UpdateTodoCollectionInput) {
+    return this.todoCollectionService.update(payload)
+  }
+
+  @Mutation(() => TodoCollection)
+  async deleteTodoCollection(
+    @Args('_id', {type: () => String}) _id: MongooseSchema.Types.ObjectId
+  ) {
+    return this.todoCollectionService.delete(_id)
+  }
+
 }
--- src/todo-collection/todo-collection.service.ts
+++ src/todo-collection/todo-collection.service.ts
@@ -1,10 +1,33 @@
 import {Injectable} from '@nestjs/common';
 import {InjectModel} from "@nestjs/mongoose";
-import {Model} from "mongoose";
+import {Model, Schema as MongooseSchema} from "mongoose";
 import {TodoCollection, TodoCollectionDocument} from "./todo-collection.model";
+import {CreateTodoCollectionInput, ListTodoCollectionInput, UpdateTodoCollectionInput} from "./todo-collection.inputs";
 
 @Injectable()
 export class TodoCollectionService {
   constructor(@InjectModel(TodoCollection.name) private todoCollectionModel: Model<TodoCollectionDocument>) {}
 
+  create(payload: CreateTodoCollectionInput) {
+    const createdTodoCollection = new this.todoCollectionModel(payload)
+    return createdTodoCollection.save()
+  }
+
+  getById(_id: MongooseSchema.Types.ObjectId) {
+    return this.todoCollectionModel.findById(_id).exec()
+  }
+
+  list(filters: ListTodoCollectionInput) {
+    return this.todoCollectionModel.find({...filters}).exec()
+  }
+
+  update(payload: UpdateTodoCollectionInput) {
+    return this.todoCollectionModel
+      .findByIdAndUpdate(payload._id, payload, {new: true})
+      .exec()
+  }
+
+  delete(_id: MongooseSchema.Types.ObjectId) {
+    return this.todoCollectionModel.findByIdAndDelete(_id).exec()
+  }
 }
