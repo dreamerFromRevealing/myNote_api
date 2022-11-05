0186f00026e2ff8660a91121f651b129b5ce42d8
Alexander Matvienko <01antizyk.it@gmail.com>
2022-11-05 12:12:09 +0200

    commit

--- src/todo-board/todo-board.module.ts
+++ src/todo-board/todo-board.module.ts
@@ -1,12 +1,15 @@
-import {Module} from '@nestjs/common';
+import {forwardRef, Module} from '@nestjs/common';
 import {MongooseModule} from "@nestjs/mongoose";
 import {TodoBoard, TodoBoardSchema} from "./todo-board.model";
 import { TodoBoardService } from './todo-board.service';
 import { TodoBoardResolver } from './todo-board.resolver';
+import {TodoBox, TodoBoxSchema} from "../todo-box/todo-box.model";
+import {TodoBoxModule} from "../todo-box/todo-box.module";
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{name: TodoBoard.name, schema: TodoBoardSchema}])
+    MongooseModule.forFeature([{name: TodoBoard.name, schema: TodoBoardSchema}, {name: TodoBox.name, schema: TodoBoxSchema}]),
+    forwardRef(() =>  TodoBoxModule)
   ],
   providers: [TodoBoardService, TodoBoardResolver],
 })
